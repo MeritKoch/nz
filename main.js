6525ec751a18729d4e197f67a9ba3e6682695d57
MeritKoch <mkoch41@smail.uni-koeln.de>
2024-04-11 15:55:50 +0200

    var durch let ersetzt

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 
-var map = L.map('map').setView([-45.345275, 170.826061], 13);
+let map = L.map('map').setView([-45.345275, 170.826061], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([-45.345275, 170.826061]).addTo(map);
+let marker = L.marker([-45.345275, 170.826061]).addTo(map);
 marker.bindPopup("<strong>Moeraki Boulders</strong><br><em>Neuseeland</em>").openPopup();
+
