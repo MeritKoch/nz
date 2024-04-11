baa394f4e680ef567ac75f2b300267a14cb004bb
MeritKoch <mkoch41@smail.uni-koeln.de>
2024-04-11 16:05:32 +0200

    lat lng und zoom definiert

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+// Koordinaten für die Karte definiert
+let lat = -45.345275;
+let lng = 170.826061;
+let zoom = 13
 
-let map = L.map('map').setView([-45.345275, 170.826061], 13);
+let map = L.map('map').setView([lat, lng], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let marker = L.marker([-45.345275, 170.826061]).addTo(map);
+let marker = L.marker([lat, lng]).addTo(map);
 marker.bindPopup("<strong>Moeraki Boulders</strong><br><em>Neuseeland</em>").openPopup();
 
