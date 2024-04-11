7ee0e54113878aeab7ad7455a5caea013266ea62
MeritKoch <mkoch41@smail.uni-koeln.de>
2024-04-11 18:06:25 +0200

    Links für alle Popups einfügen

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ L.control.scale({
 }).addTo(map);
 
 //GeoJSON aller Punkte des Kurses hinzugefügt
-let jsonPunkt = {
+let jsondata = {
     "type": "FeatureCollection",
     "features": [
         {
@@ -286,13 +286,16 @@ let jsonPunkt = {
 };
 
 //Leaflet einfügen
-L.geoJSON(jsonPunkt, {}).bindPopup(function (layer) {
+L.geoJSON(jsondata, {}).bindPopup(function (layer) {
+    console.log(layer.feature.properties)
     return `
     <h2>${layer.feature.properties.name}
     <h2>
-    <em> <ul>
+    <ul>
         <li>Breite: ${layer.feature.geometry.coordinates[1].toFixed(5)}</li>
         <li>Länge: ${layer.feature.geometry.coordinates[0].toFixed(5)} </li>
-    </ul> </em>
+        <li><a href="https://${layer.feature.properties.user}.github.io/nz">Etappenseite</a></li>
+        <li><a href="${layer.feature.properties.wikipedia}">Wikipedia</li>
+    </ul>
     `;
 }).addTo(map);
